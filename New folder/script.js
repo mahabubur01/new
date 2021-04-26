2b869f7261f4abce83f2651edbd3c9bc8a3f5ddd
mahabubur01 <76788813+mahabubur01@users.noreply.github.com>
2021-04-27 05:40:56 +0600

    github

---
+++ New folder/script.js
@@ -0,0 +1,22 @@
+/* tutorial 11
+var number1 = 5;
+var number2 = 20;
+var result= ++ number1 /2;
+document.querySelector('#showkoro').innerHTML= result ;
+*/
+
+ tutorial 12
+
+var x = " Hello world" ;
+var x = 123;
+var x = ["Sumon", "Mahbub", "Nasim"];
+var x = {Name:"Mahbub", Address:"lalmonirhat"};
+var x = /*true*/ false;
+var x = undefined ;
+var x = null;
+document.querySelector('#showkoro').innerHTML = typeof x ;
+
+
+
+/* tutorial 13 (javascript conparison operators)
+*/
